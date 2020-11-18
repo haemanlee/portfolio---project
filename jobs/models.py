7fe9d4ec64ae27ceed74d4e77ae53d52c422bdfb
이해만 (Hae-Man LEE) <2016110@clt.net>
2020-11-18 18:17:43 +0900

    admin setting

--- jobs/models.py
+++ jobs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
-class Job(model.Model):
-    
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+
+MEDIA_URL = '/coolstuff/'
